ea04bf9274ccc664c3f81387219031aa355c29b9
ltomasinbarbosa <143366644+ltomasinbarbosa@users.noreply.github.com>
2023-09-11 22:25:16 -0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,39 +1,53 @@
 import { Bike } from "./bike";
 import { Rent } from "./rent";
 import { User } from "./user";
-import { genSaltSync, hashSync } from 'bcrypt';
-// import crypto from 'crypto'
+import crypto from "crypto";
+import * as bcrypt from 'bcrypt';
+import { Time } from "./time";
+import { time } from "console";
 
 export class App {
     users: User[] = []
     bikes: Bike[] = []
     rents: Rent[] = []
-    salt = genSaltSync(10)
 
     findUser(email: string): User | undefined {
-        return this.users.find(user => user.email === email)
+        return this.users.find(user => { return user.email === email })
     }
-
-    registerUser(user: User): string {
-        for (const rUser of this.users) {
-            if (rUser.email === user.email) {
-                throw new Error('Duplicate user.')
-            }
+    async registerUser(user: User): Promise<string> {
+        if (this.users.some(rUser => { return rUser.email === user.email })) {
+            throw new Error('User with the same email already registered.')
         }
+        await user.setPassword(user.password); // Chame setPassword antes de adicionar o usuário
         const newId = crypto.randomUUID()
         user.id = newId
-        var hash = hashSync(user.passoword, this.salt);
-        user.passoword = hash;
-        this.users.push(user)
+        this.users.push(user);
         return newId
     }
-
+    
     registerBike(bike: Bike): string {
         const newId = crypto.randomUUID()
         bike.id = newId
         this.bikes.push(bike)
         return newId
     }
+
+
+    rentBike(bikeId: string, userEmail: string, start: Time): void {
+        const bike = this.bikes.find(bike => bike.id === bikeId)
+        if (!bike) {
+            throw new Error('Bike not found.')
+        }
+        const user = this.findUser(userEmail)
+        if (!user) {
+            throw new Error('User not found.')
+        }
+       
+        const newRent = Rent.create(bike, user,start)
+        this.rents.push(newRent)
+    }
+
+
 
     removeUser(email: string): void {
         const userIndex = this.users.findIndex(user => user.email === email)
@@ -43,86 +57,53 @@ export class App {
         }
         throw new Error('User does not exist.')
     }
-    
-    rentBike(bikeId: string, userEmail: string, startDate: Date, endDate: Date): void {
-        const bike = this.bikes.find(bike => bike.id === bikeId)
-        if (!bike) {
-            throw new Error('Bike not found.')
-        }
-        const user = this.findUser(userEmail)
-        if (!user) {
-            throw new Error('User not found.')
-        }
-        const bikeRents = this.rents.filter(rent =>
-            rent.bike.id === bikeId && !rent.dateReturned
-        )
-        const newRent = Rent.create(bikeRents, bike, user, startDate, endDate)
-        this.rents.push(newRent)
-    }
 
-    returnBike(bikeId: string, userEmail: string) {
-        const today = new Date()
-        const rent = this.rents.find(rent => 
+
+
+    returnBike(bikeId: string, userEmail: string,returnTime: Time, start: Time): Number {
+        const rent = this.rents.find(rent =>
             rent.bike.id === bikeId &&
-            rent.user.email === userEmail &&
-            rent.dateReturned === undefined &&
-            rent.dateFrom <= today
+            rent.user.email === userEmail
         )
         if (rent) {
-            rent.dateReturned = today
-            return
+            rent.end = returnTime
+            rent.valor = rent.valorTot(start, returnTime)
+            return rent.valorTot(start, returnTime)
         }
         throw new Error('Rent not found.')
     }
 
-    ListUser(): void{
-        this.users.some(pUser => {
-            console.log("Nome: " + pUser.name)
-            console.log("Email: " + pUser.email)
-            console.log("Id: " + pUser.id)
-            console.log("Senha: " + pUser.passoword + "\n")
-        })
-    }
-
-    ListRent(): void{
-        this.rents.some(pRent => {
-            console.log("User: " + pRent.user.name)
-            console.log("Bike: " + pRent.bike.name)
-            console.log("Date from: " + pRent.dateFrom)
-            console.log("Date to: " + pRent.dateTo)
-            console.log("Date returned: " + pRent.dateReturned + "\n")
-        })
-    }
-
-    ListBike(): void{
-        this.bikes.some(pBike => {
-            console.log("Nome: " + pBike.name)
-            console.log("Type: " + pBike.type)
-            console.log("Body Size: " + pBike.bodySize)
-            console.log("Max Load: " + pBike.maxLoad)
-            console.log("Rate: " + pBike.rate)
-            console.log("Description: " + pBike.description)
-            console.log("Ratings: " + pBike.ratings)
-            console.log("Id: " + pBike.id + "\n")
-        })
-    }
-
-    //função retorna verdadeiro caso esteja tudo certo, e falso se não
-    Authentication(id: string, senha: string){
-        for(let i = 0; i < this.users.length; i++){
-            if(this.users[i].id === id){
-                var verifica: string = hashSync(senha, this.salt);
-                console.log(verifica)
-                if(verifica === this.users[i].passoword){
-                    return true
-                }
-                else{
-                    throw new Error('Senha errada.')
-                }
-            }
+    listUsers(): User[] {
+        if(this.users.length === 0){
+            throw new Error("Nenhum usuário cadastrado")
         }
-        throw new Error('Email não cadastrado.')
+        return this.users;
     }
+
+    listRent(): Rent[]{
+        if(this.rents.length === 0){
+            throw new Error("Nenhum aluguel cadastrado")
+        }
+        return this.rents;
+    }
+
+    listBikes(): Bike[]{
+        if(this.bikes.length === 0){
+            throw new Error("Nenhuma bike cadastrada")
+        }
+        return this.bikes;
+    }
+
+    async authenticateUser(userEmail: string, password: string): Promise<boolean> {
+        const user = this.findUser(userEmail);
+        if (user) {
+            const passwordMatch = await user.checkPassword(password);
+            return passwordMatch;
+        }
+        return false;
+    }
+
+
 }
 
         // for(const rUser of this.users){
